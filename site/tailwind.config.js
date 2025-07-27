070e19d396a6920c89b08a8e2ec00d289ebfa389
Tristan Jessup <jessupjdj@outlook.com>
2025-07-26 20:08:00 -0400

    Fix Tailwind PostCSS config

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -1,5 +1,7 @@
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: { extend: {} },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"] ,
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
